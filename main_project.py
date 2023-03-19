2dea5b904e7b336dca485fbfe42c159602d90314
JasonWu00 <zehong.wu@macaulay.cuny.edu>
2023-03-19 16:30:57 -0400

    new python code to clean second data set

--- main_project.py
+++ main_project.py
@@ -18,7 +18,7 @@ def import_housing(csv_name, columns_to_use):
     The data in the .csv file is read into a DataFrame.
     If a valid list of columns is provided, only those columns will be kept.
 
-    Returns the DataFrame.
+    Returns an imputed version of the DataFrame.
     """
     df = None
     df = pd.read_csv(csv_name)
@@ -97,9 +97,9 @@ def impute_housing(df):
     df["Percent"] = df["Percent"].apply(lambda percent: int(percent*100))
     return df
 
-def main():
+def clean_store_ahs_data():
     """
-    main function
+    Step 1: clean up the Affordable Housing data and save it to a csv file.
     """
     desired_columns = [ "Project ID", "Project Start Date", "Project Completion Date",\
                         "Borough", "Postcode", "Census Tract",\
@@ -119,6 +119,83 @@ def main():
 
     #df_housing = pd.read_csv("AHP_by_Building_cleaned.csv")
     print(df_housing)
+
+def strip_letters(index):
+    """
+    This function takes one input:
+    index: a string for a DataFrame index.
+
+    Returns a version of index with only numbers.
+
+    Example:
+    input: 'ZCTAS 10101 Estimate'
+    Returns: '10101'
+    """
+    output = ""
+    for char in index:
+        if "0" <= char <= "9":
+            output += char
+    return output[1:]
+
+def import_income(csv_name):
+    """
+    This function takes one input:
+    csv_name: the name of a .csv file to read.
+
+    The data in the .csv file is read into a DataFrame.
+    The DataFrame undergoes the following changes:
+    - Rotate 90 degrees
+    - Rename indices and columns for easier reading
+    - Drop some useless columns
+
+    Returns the modified DataFrame.
+    """
+    df_zip_income = pd.read_csv(csv_name, index_col=[0])
+    #print(df_zip_income)
+    # for column_label in df_zip_income:
+    #     print("Printing a column label")
+    #     print(column_label)
+    df_zip_income_transposed = df_zip_income.transpose()
+    #print(df_zip_income_transposed)
+
+    # rename indices
+    rename_index_dict = {}
+    for idx in df_zip_income_transposed.index:
+        if idx == "New York city, New York!!Households!!Estimate":
+            rename_index_dict[idx] = "NYC"
+        else:
+            rename_index_dict[idx] = strip_letters(idx)
+    df_zip_income_transposed.rename(index=rename_index_dict, inplace=True)
+
+    # rename columns
+    rename_column_dict = {}
+    for col in df_zip_income_transposed.columns:
+        rename_column_dict[col] = col.strip()
+    df_zip_income_transposed.rename(columns=rename_column_dict, inplace=True)
+
+    # drop useless columns
+    # print(df_zip_income_transposed)
+    # print(df_zip_income_transposed.columns)
+    columns_to_drop = ["PERCENT ALLOCATED", "Family income in the past 12 months",\
+                       "Nonfamily income in the past 12 months"]
+    df_zip_income_transposed.drop(columns=columns_to_drop, inplace=True)
+
+    df_zip_income_transposed.index.name = "Zipcode"
+    return df_zip_income_transposed
+
+def clean_store_income_data():
+    """
+    Step 2: clean up the 2021 Income by ZIP data and save it to a csv file.
+    """
+    df_zip_income = import_income("NYC_income_by_zip_2021_untransposed_truncated.csv")
+    df_zip_income.to_csv("NYC_Income_Brackets_by_ZIP_cleaned.csv", index=True)
+
+def main():
+    """
+    main function
+    """
+    # clean_store_ahs_data()
+    clean_store_income_data()
 
 if __name__ == "__main__":
     main()
