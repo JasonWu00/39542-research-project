c884cad42d32dd8e61377a70f75701938a5957d8
JasonWu00 <zehong.wu@macaulay.cuny.edu>
2023-03-19 01:38:24 -0400

    step 1: cleaning AHP data

--- main_project.py
+++ main_project.py
@@ -1,7 +1,8 @@
 """
 Name: Ze Hong Wu
 Email: zehong.wu@macaulay.cuny.edu
-Resources: -
+Resources:
+Pandas documentation for all manner of debugging help
 Title: -
 URL: -
 """
@@ -20,23 +21,23 @@ def import_data(csv_name, columns_to_use):
     Returns the DataFrame.
     """
     df = None
+    df = pd.read_csv(csv_name)
     if columns_to_use != []:
-        df = pd.read_csv(csv_name, columns_to_use)
-    else:
-        df = pd.read_csv(csv_name)
+        df = df[columns_to_use]
+    #print("Finished importing; printing some sample columns")
     return impute_housing(df)
 
 def choose_date(date, default_date):
     """
     This function takes two inputs:
     date: a potentially NaN date.
-    default_date: a non-NaN default date to return..
+    default_date: a non-NaN default date to return.
 
     Returns date if it is not NaN, or default_date otherwise.
     """
     if pd.isna(date):
-        return default_date
-    return date
+        return pd.to_datetime(default_date)
+    return pd.to_datetime(date)
 
 def choose_postcode(postcode, boro):
     """
@@ -47,6 +48,7 @@ def choose_postcode(postcode, boro):
     Returns postcode if it is not empty, or the ZIP corresponding to boro otherwise.
     Based on impute_zip() in Assignment 4.
     """
+    #print("Choosing postcode or boro")
     boro_dict = {"Bronx": 10451,
                 "Brooklyn": 11201,
                 "Manhattan": 10001,
@@ -65,6 +67,10 @@ def impute_housing(df):
     Project Completion Date: January 1, 2024
     Postcode: the default post-codes of each borough
 
+    The following columns will be added to the DataFrame:
+    Project Start Year: year project starts
+    Project End Year: year project ends
+
     The imputing will be done "manually" (unique code for every column).
     This is bad coding form, but circumstances make a more "elegant" solution difficult.
 
@@ -72,9 +78,22 @@ def impute_housing(df):
     """
 
     df["Project Completion Date"] = df["Project Completion Date"]\
-                                    .apply(lambda date: choose_date(date,\
-                                            pd.to_datetime("2024-01-01")))
-    df["Postcode"] = df.apply(lambda row: choose_postcode(row["Postcode"], row["Borough"]))
+                                .apply(lambda date: choose_date(date,"01/01/2024"))
+    df["Project Start Date"] = df["Project Start Date"]\
+                                .apply(lambda date: choose_date(date,"01/01/2024"))
+
+    df["Postcode"] = df.apply(lambda row: choose_postcode(row["Postcode"], row["Borough"]), axis=1)
+    df["Postcode"] = df["Postcode"].apply(int)
+
+    df["Project Start Year"] = df["Project Start Date"]\
+                            .apply(lambda date: date.year)
+
+    df["Project End Year"] = df["Project Completion Date"]\
+                            .apply(lambda date: date.year)
+
+    df["Percent"] = 0.0
+    df["Percent"] = df["All Counted Units"] / df["Total Units"]
+    df["Percent"] = df["Percent"].apply(lambda percent: int(percent*100))
     return df
 
 def main():
@@ -88,12 +107,17 @@ def main():
                         "Extended Affordability Only", "Extremely Low Income Units",\
                         "Very Low Income Units", "Low Income Units", "Middle Income Units",\
                         "All Counted Units", "Total Units"]
-    df = import_data("Affordable_Housing_Production_by_Building.csv", desired_columns)
+    # print("Before import")
+    # print(desired_columns)
+    df_housing = import_data("Affordable_Housing_Production_by_Building.csv", desired_columns)
 
     # saving intermediate work to .csv files
     # to skip earlier work in later sections
     # to do everything in one go, cmment out all save lines
-    df.to_csv("AHP_by_Building_cleaned.csv")
+    df_housing.to_csv("AHP_by_Building_cleaned.csv", index=False)
+
+    #df_housing = pd.read_csv("AHP_by_Building_cleaned.csv")
+    print(df_housing)
 
 if __name__ == "__main__":
     main()
